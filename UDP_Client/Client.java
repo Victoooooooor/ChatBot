a5a04ee66b9f468ace29fea7483b593db455eca2
Victor LEFEVRE <victorlefevre0@gmail.com>
2024-05-31 20:52:04 +0200

    Commentaires

--- UDP_Client/Client.java
+++ UDP_Client/Client.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.io.IOException;
-import java.net.DatagramPacket;
+import java.net.DatagramPacket; 
 import java.net.InetAddress;
 
 /**
